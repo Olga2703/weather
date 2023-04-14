18af969a100c914a957aea839b4c1f30be74d01c
Olga Petrenko <smirnova_olga18@mail.ru>
2023-04-14 16:46:20 +0400

    fix: search input

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import routes from "../routes.js";
 import "../style/homePage.css";
-import CityFavorite from "./CityFavorite";
+// import CityFavorite from "./CityFavorite";
+import { selectors, fetchCities } from "../slices/citiesSlice.js";
+
 const HomePage = () => {
+  const cities = useSelector(selectors.selectAll);
+  const dispatch = useDispatch();
+  console.log(cities);
   return (
     <div className="container-fluid">
       <Form className="search-form d-flex justify-content-center">
         <Form.Group className="mb-3 search-form__input">
-          <Form.Control type="search" placeholder="Укажите город" />
+          <Form.Control
+            type="search"
+            placeholder="Укажите город"
+            onChange={(e) => dispatch(fetchCities(e.target.value))}
+          />
         </Form.Group>
       </Form>
       <section className="tip d-flex justify-content-center flex-column align-items-center">
--- src/slices/citiesSlice.js
+++ src/slices/citiesSlice.js
@@ -4,6 +4,7 @@ import {
   createEntityAdapter,
 } from "@reduxjs/toolkit";
 import axios from "axios";
+import uniqid from "uniqid";
 
 const API_KEY = "215d517a61f27aeb7e43b325b15e0887";
 
@@ -13,10 +14,13 @@ const initialState = citiesAdapter.getInitialState();
 export const fetchCities = createAsyncThunk(
   "cities/fetchCities",
   async (city) => {
+    if (city.trim().length === 0) {
+      return [];
+    }
     const response = await axios.get(
       `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=20&appid=${API_KEY}`
     );
-    return response;
+    return response.data;
   }
 );
 
@@ -26,6 +30,20 @@ const citiesSlice = createSlice({
   reducers: {
     addCity: citiesAdapter.addOne,
     addCities: citiesAdapter.addMany,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCities.fulfilled, (state, action) => {
+      if (action.payload.length === 0) {
+        citiesAdapte.
+      }
+      action.payload.forEach((city) => {
+        const id = uniqid();
+        state.entities[id] = {
+          name: city.local_names.ru ?? city.name,
+        };
+        state.ids.push(id);
+      });
+    });
   },
 });
 
